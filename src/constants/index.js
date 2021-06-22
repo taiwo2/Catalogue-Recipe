a069473060afdae1f62a9ca626d62c8f984040c0
taiwo2 <adetonat@ymail.com>
2021-06-22 19:44:33 +0100

    Delete files

--- src/constants/index.js
+++
@@ -1,8 +0,0 @@
-export const CATEGORY_FILTERS = {
-  ALL: 'All',
-};
-
-export const URL = {
-  RECIPES: 'https://www.themealdb.com/api/json/v1/1/search.php?s=',
-  CATEGORIES: 'https://www.themealdb.com/api/json/v1/1/categories.php',
-};
