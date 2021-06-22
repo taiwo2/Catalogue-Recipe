1f6160b345cd5bc3df6b4debd88aed7cba4e73ae
taiwo2 <adetonat@ymail.com>
2021-06-22 18:08:58 +0100

    Add style

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Menu from '../assets/images/menu.png';
 
-const MenuComponent = props => {
+const MenuComponent = (props) => {
   const { handleClick } = props;
   return (
-    <button className="btn-icon" type="button" onClick={e => handleClick(e)}>
+    <button className="btn-icon" type="button" onClick={(e) => handleClick(e)}>
       <img
         className="menu-icon"
         src={Menu}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './redux/store';
+import './assets/styles.css';
 
 ReactDOM.render(
   <Provider store={store}>
